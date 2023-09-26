97d2d787f78c10fb910d128b930acfc7dd93d0ee
najwalahly <najwa.lahly@gmail.com>
2023-09-26 13:51:44 +0200

    add completed class if todo is checked

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,8 +1,13 @@
+import { useState } from "react"
+
 export default function Todo({todo}) {
+    const [isChecked, setIsChecked] = useState(false);
+
+
     return (
-        <li>
+        <li className={isChecked && "completed"}>
             <div className="view">
-				<input className="toggle" type="checkbox" unchecked></input>
+				<input className="toggle" type="checkbox" onChange={() => setIsChecked(!isChecked)}></input>
 				<label>{todo}</label>
 				<button className="destroy"></button>
 			</div>
