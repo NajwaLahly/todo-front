f3f8ac3bbeabdbc40e641327b7e4663dc148bf37
najwalahly <najwa.lahly@gmail.com>
2023-09-27 15:10:40 +0200

    refacto, add display mode : norma / edit

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,52 +1,93 @@
 import { useState } from "react";
 
-export default function Todo({ todo , handleEdit, handleDestroy}) {
+export default function Todo({ todo, handleEdit, handleDestroy }) {
   const [isChecked, setIsChecked] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [inputText, setInputText] = useState(todo.value);
 
-  const doubleClickHandler = () => {
+  const handleDoubleClick = () => {
     setIsEditing(!isEditing);
   };
 
   const handleBlur = () => {
     setIsEditing(false);
-    handleEdit({id:todo.id, value: inputText})
+    handleEdit({ id: todo.id, value: inputText });
   };
 
   const handleKeyPress = (e) => {
-    if(e.key === "Enter") {
+    if (e.key === "Enter") {
       handleBlur();
-    } 
-    if(e.key === "Escape") {
+    }
+    if (e.key === "Escape") {
       setIsEditing(false);
     }
-  }
+  };
 
+  const handleChange = (e) => setInputText(e.target.value);
 
   return (
-    <li
-      className={`${isChecked && "completed"} ${isEditing && "editing"}`}
-    >
+    <>
+      {isEditing ? (
+        <TodoEditDisplay
+          inputText={inputText}
+          handleBlur={handleBlur}
+          handleKeyPress={handleKeyPress}
+          handleChange={handleChange}
+        />
+      ) : (
+        <TodoNormalDisplay
+          isChecked={isChecked}
+          setIsChecked={setIsChecked}
+          handleDoubleClick={handleDoubleClick}
+          todo={todo}
+          handleDestroy={handleDestroy}
+        />
+      )}
+    </>
+  );
+}
+
+function TodoNormalDisplay({
+  isChecked,
+  setIsChecked,
+  handleDoubleClick,
+  todo,
+  handleDestroy,
+}) {
+  return (
+    <li className={`${isChecked && "completed"}`}>
       <div className="view">
         <input
           className="toggle"
           type="checkbox"
           onChange={() => setIsChecked(!isChecked)}
         ></input>
-        <label onDoubleClick={doubleClickHandler} >{todo.value}</label>
-        <button className="destroy" onClick={() => handleDestroy(todo.id)}></button>
+        <label onDoubleClick={handleDoubleClick}>{todo.value}</label>
+        <button
+          className="destroy"
+          onClick={() => handleDestroy(todo.id)}
+        ></button>
       </div>
-      {isEditing && (
-        <input
-          className="edit"
-          value={inputText}
-          onBlur={handleBlur}
-          onKeyDown={handleKeyPress}
-          onChange={(e) => setInputText(e.target.value)}
-          autoFocus
-        ></input>
-      )}
+    </li>
+  );
+}
+
+function TodoEditDisplay({
+  inputText,
+  handleBlur,
+  handleKeyPress,
+  handleChange,
+}) {
+  return (
+    <li className="editing">
+      <input
+        className="edit"
+        value={inputText}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyPress}
+        onChange={handleChange}
+        autoFocus
+      ></input>
     </li>
   );
 }
